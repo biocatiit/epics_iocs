76d47e586c5096759a6604ad405770a2cebb0b68
jessehopkins <hopkins.jesse@gmail.com>
2024-10-17 09:57:12 -0500

    Moved 18id_con to bleps top level folder.

--- soft_iocs/bleps/18idApp/src/O.linux-x86_64/ioc18idLinux_registerRecordDeviceDriver.cpp
+++ soft_iocs/bleps/18idApp/src/O.linux-x86_64/ioc18idLinux_registerRecordDeviceDriver.cpp
@@ -836,7 +836,7 @@ static struct iocshVarDef vardefs[] = {
 int ioc18idLinux_registerRecordDeviceDriver(DBBASE *pbase)
 {
     static int executed = 0;
-    const char *bldTop = "/opt/epics/iocs/epics_iocs/soft_iocs/BLEEPS";
+    const char *bldTop = "/opt/epics/iocs/epics_iocs/soft_iocs/bleps";
     const char *envTop = getenv("TOP");
 
     if (envTop && strcmp(envTop, bldTop)) {
