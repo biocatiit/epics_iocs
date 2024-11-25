2ab35d65ea2473d9c17210ff4c338673a859724f
jessehopkins <hopkins.jesse@gmail.com>
2024-11-25 15:58:48 -0600

    Fixed a typo in newport shortcuts.

--- shortcuts/start_ioc_newport_d_hutch.sh
+++ shortcuts/start_ioc_newport_d_hutch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pushd /opt/epics/epics_iocs/soft_iocs/nepwort/iocBoot/iocnewport_d_hutch
+pushd /opt/epics/epics_iocs/soft_iocs/newport/iocBoot/iocnewport_d_hutch
 ./ioc_start.sh $1
 popd
--- shortcuts/start_ioc_newport_tr.sh
+++ shortcuts/start_ioc_newport_tr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pushd /opt/epics/epics_iocs/soft_iocs/nepwort/iocBoot/iocnewport_tr
+pushd /opt/epics/epics_iocs/soft_iocs/newport/iocBoot/iocnewport_tr
 ./ioc_start.sh $1
 popd
