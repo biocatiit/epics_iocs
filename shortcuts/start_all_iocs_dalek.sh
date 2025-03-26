3f4dc757ee5c4f1a2cc078efb42c23c9c4d2da5e
jessehopkins <hopkins.jesse@gmail.com>
2025-03-26 14:43:01 -0500

    Added new autosave files.

--- shortcuts/start_all_iocs_dalek.sh
+++ shortcuts/start_all_iocs_dalek.sh
@@ -6,6 +6,7 @@ pushd /opt/epics/epics_iocs/shortcuts
 ./start_ioc_galil_dmc_e03.sh start
 ./start_ioc_galil_dmc_e04.sh start
 ./start_ioc_galil_dmc_e05.sh start
+./start_ioc_galil_dmc_a01.sh start
 ./start_ioc_hutch_temp_monitor.sh start
 ./start_ioc_labjack.sh start
 ./start_ioc_meas_comp.sh start
--- shortcuts/stop_all_iocs_dalek.sh
+++ shortcuts/stop_all_iocs_dalek.sh
@@ -6,6 +6,7 @@ pushd /opt/epics/epics_iocs/shortcuts
 ./start_ioc_galil_dmc_e03.sh stop
 ./start_ioc_galil_dmc_e04.sh stop
 ./start_ioc_galil_dmc_e05.sh stop
+./start_ioc_galil_dmc_a01.sh stop
 ./start_ioc_hutch_temp_monitor.sh stop
 ./start_ioc_labjack.sh stop
 ./start_ioc_meas_comp.sh stop
