788b699d2419293369f50dea9239c863403b9934
jbhopkins <hopkins.jesse@gmail.com>
2026-02-09 10:48:27 -0600

    Added autosave to toaster ioc.

--- soft_iocs/toaster/iocBoot/ioctoaster/st.cmd
+++ soft_iocs/toaster/iocBoot/ioctoaster/st.cmd
@@ -14,15 +14,23 @@ dbLoadDatabase "../../dbd/toaster.dbd"
 toaster_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-# Horizontal motor toaster. Change MOTOR=pv_name to the PV name for the motor being used.
+# Note that the MOTOR value for toast.db is actually most likely being set by the autosave
+# So if you change motor here in the database be sure to delete autosave files for this to work
+# Or just change it by setting the PV.LNK1 and LNK2 values as usual once the IOC is running
+# If you do change it while IOC is running, make sure LNK1 and LNK2 are the same!
+
+# Horizontal motor toaster. 
 dbLoadRecords("toast.db", "P=18ID:,D=H:,MOTOR=18ID_DMC_E05:33")
 
-# Vertical motor toaster. Change MOTOR=pv_name to the PV name for the motor being used.
-#dbLoadRecords("toast.db", "P=18ID:,D=V:,MOTOR=18ID_DMC_E05:34")
+# Vertical motor toaster.
+dbLoadRecords("toast.db", "P=18ID:,D=V:,MOTOR=18ID_DMC_E05:34")
+
+# Autosave cmd
+< save_restore.cmd
 
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=biocat"
+# Start autosave
+create_monitor_set("auto_settings.req", 30,"P=18ID:")
 
 date
